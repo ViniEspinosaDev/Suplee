daada6795f2e30c6f00248c3158bc54ce834a4db
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2022-05-29 10:27:02 -0300

    CORS

--- src/Suplee.Catalogo.Api/Startup.cs
+++ src/Suplee.Catalogo.Api/Startup.cs
@@ -65,7 +65,10 @@ namespace Suplee.Catalogo.Api
             var ips = new List<string>()
                 {
                     "https://supleeapiv1.herokuapp.com/",
+                    "http://192.168.1.70:3000/",
+                    "https://192.168.1.70:3000/",
                     "http://192.168.1.73:3000/",
+                    "https://192.168.1.73:3000/",
                     "https://suplee.vercel.app/"
                 };
 
