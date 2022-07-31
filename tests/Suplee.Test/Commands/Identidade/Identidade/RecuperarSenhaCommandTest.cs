391282f6adbbc2eab9980b9a05fd4846276a928d
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2022-07-30 23:09:54 -0300

    Comando de recuperar senha testado

--- tests/Suplee.Test/Commands/Identidade/Identidade/RecuperarSenhaCommandTest.cs
+++ tests/Suplee.Test/Commands/Identidade/Identidade/RecuperarSenhaCommandTest.cs
@@ -1,4 +1,10 @@
-﻿using Suplee.Test.Builder.Commands.Identidade.Identidade;
+﻿using Moq;
+using Suplee.Core.Messages.CommonMessages.Notifications;
+using Suplee.Identidade.Domain.Models;
+using Suplee.Test.Builder.Commands.Identidade.Identidade;
+using Suplee.Test.Builder.Models;
+using System;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Suplee.Test.Commands.Identidade.Identidade
@@ -31,7 +37,69 @@ namespace Suplee.Test.Commands.Identidade.Identidade
         #endregion
 
         #region Validações de negócios
+        [Fact]
+        public async Task Deve_Validar_Handler_Comando_Invalido()
+        {
+            var comando = new RecuperarSenhaCommandBuilder().ComandoInvalido().Build();
 
+            var resultado = await _handler.Handle(comando, _cancellationToken);
+
+            _usuarioRepository.Verify(x => x.UnitOfWork.Commit(), Times.Never);
+            _mediatorHandler.Verify(x => x.PublicarNotificacao(It.IsAny<DomainNotification>()), Times.AtLeastOnce);
+
+            Assert.Empty(resultado);
+        }
+
+        [Fact]
+        public async Task Deve_Nao_Encontrar_Usuario_Pelo_CPF()
+        {
+            var comando = new RecuperarSenhaCommandBuilder().ComandoValido().Build();
+
+            _usuarioRepository.Setup(x => x.ObterPeloCPF(It.IsAny<string>())).Returns(default(Usuario));
+
+            var resultado = await _handler.Handle(comando, _cancellationToken);
+
+            _usuarioRepository.Verify(x => x.UnitOfWork.Commit(), Times.Never);
+            _mediatorHandler.Verify(x => x.PublicarNotificacao(It.Is<DomainNotification>(x => x.Value == "Não existe nenhum usuário cadastrado com esse CPF")), Times.Once);
+
+            Assert.Empty(resultado);
+        }
+
+        [Fact]
+        public async Task Deve_Encontrar_Usuario_Status_Aguardando_Confirmacao()
+        {
+            var usuario = new UsuarioBuilder().PadraoValido().Build();
+
+            var comando = new RecuperarSenhaCommandBuilder().ComandoValido().Build();
+
+            _usuarioRepository.Setup(x => x.ObterPeloCPF(It.IsAny<string>())).Returns(usuario);
+
+            var resultado = await _handler.Handle(comando, _cancellationToken);
+
+            _usuarioRepository.Verify(x => x.UnitOfWork.Commit(), Times.Never);
+            _mediatorHandler.Verify(x => x.PublicarNotificacao(It.Is<DomainNotification>(x => x.Value == "O Usuário ainda não está com o cadastro confirmado. Verifique o e-mail")), Times.Once);
+
+            Assert.Empty(resultado);
+        }
+
+        [Fact]
+        public async Task Deve_Nao_Encontrar_Confirmacao_Usuario()
+        {
+            var usuario = new UsuarioBuilder().PadraoValido().Build();
+
+            usuario.Ativar();
+
+            var comando = new RecuperarSenhaCommandBuilder().ComandoValido().Build();
+
+            _usuarioRepository.Setup(x => x.ObterPeloCPF(It.IsAny<string>())).Returns(usuario);
+            _usuarioRepository.Setup(x => x.ObterConfirmacaoUsuario(It.IsAny<Guid>(), It.IsAny<string>())).Returns(default(ConfirmacaoUsuario));
+            _usuarioRepository.Setup(x => x.UnitOfWork.Commit()).Returns(Task.FromResult(true));
+
+            var resultado = await _handler.Handle(comando, _cancellationToken);
+
+            Assert.NotEmpty(resultado);
+            Assert.Equal(usuario.Email, resultado);
+        }
         #endregion
     }
 }
